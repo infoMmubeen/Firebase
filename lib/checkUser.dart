759673fb511ef0a0109c248dac2208bbafc1244b
Buy_Nexus <infomuhammadmubeen@gmail.com>
2024-09-28 01:01:08 +0500

    Ensure user is kept logged in based on authentication state

---
+++ lib/checkUser.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:new_after_slow_fire/home.dart';
+
+import 'loginPage.dart';
+
+class Checkuser extends StatefulWidget {
+  const Checkuser({super.key});
+
+  @override
+  State<Checkuser> createState() => _CheckuserState();
+}
+
+class _CheckuserState extends State<Checkuser> {
+  Future<Widget> checkuser() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return HomePage(); // Navigate to HomePage if user is logged in
+    } else {
+      return Loginpage(); // Navigate to LoginPage if no user is logged in
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Widget>(
+      future: checkuser(),
+      builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
+        // Checking if the future has data
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show a loading spinner while waiting
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          // Show the determined page when the future is done
+          return snapshot.data!;
+        } else {
+          // Handle errors or unexpected nulls
+          return Scaffold(
+            body: Center(
+              child: Text('Error checking user status'),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
